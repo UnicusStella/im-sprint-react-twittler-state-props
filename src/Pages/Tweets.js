594b768c4ae1fbf488e2bbde57a2afea57e072de
UnicusStella <unicusstella@gmail.com>
2021-07-13 13:54:57 +0900

    modify

--- src/Pages/Tweets.js
+++ src/Pages/Tweets.js
@@ -7,24 +7,38 @@ import dummyTweets from '../static/dummyData';
 
 const Tweets = () => {
   // TODO : 새로 트윗을 작성하고 전송할 수 있게 useState를 적절히 활용하세요.
-  const [tweets, setTweets] = useState(dummyTweets);
-  const [userinput, setUserinput] = useState('');
-  const [message, setMessage] = useState('');
+  const [정보목록, 정보목록변경] = useState(dummyTweets);
+  const [유저이름, 유저이름변경] = useState('');
+  const [내용, 내용변경] = useState('');
+
+  function set(x) {
+    y = x;
+    return y;
+  }
 
   const handleButtonClick = (event) => {
-    const tweet = {};
+    const tweet = {
+      id: 9,
+      username: 유저이름,
+      picture: `https://randomuser.me/api/portraits/men/98.jpg`,
+      content: 내용,
+      createdAt: '2019-02-25T16:17:47.000Z',
+      updatedAt: '2019-02-25T16:17:47.000Z',
+    };
+
     // TODO : Tweet button 엘리먼트 클릭시 작동하는 함수를 완성하세요.
     // 트윗 전송이 가능하게 작성해야 합니다.
+    정보목록변경([tweet, ...정보목록]);
   };
 
   const handleChangeUser = (event) => {
-    // TODO : Tweet input 엘리먼트에 입력 시 작동하는 함수를 완성하세요.
-    setUserinput(event.target.userinput);
+    // TODO : Tweet input 엘리먼트에  입력 시 작동하는 함수를 완성하세요.
+    유저이름변경(event.target.value);
   };
 
   const handleChangeMsg = (event) => {
     // TODO : Tweet textarea 엘리먼트에 입력 시 작동하는 함수를 완성하세요.
-    setMessage(event.target.value);
+    내용변경(event.target.value);
   };
 
   return (
@@ -38,24 +52,25 @@ const Tweets = () => {
             <div className="tweetForm__inputWrapper">
               <div className="tweetForm__input">
                 <input
-                  type="text"
-                  defaultValue={userinput}
-                  placeholder="your username here.."
                   className="tweetForm__input--username"
+                  type="text"
+                  placeholder="your username here.."
+                  defaultValue={유저이름}
                   onChange={handleChangeUser}
                 ></input>
                 {/* TODO : 트윗을 작성할 수 있는 textarea 엘리먼트를 작성하세요. */}
                 <textarea
                   className="tweetForm__input--message"
                   type="text"
-                  defaultValue={message}
+                  placeholder="your message here.."
+                  defaultValue={내용}
                   onChange={handleChangeMsg}
                 ></textarea>
               </div>
               <div className="tweetForm__count" role="status">
                 <span className="tweetForm__count__text">
                   {/* TODO : 트윗 총 개수를 보여줄 수 있는 Counter를 작성하세요. */}
-                  total : {tweets.length}
+                  total : {정보목록.length}
                 </span>
               </div>
             </div>
@@ -64,7 +79,9 @@ const Tweets = () => {
               {/* TODO : 작성한 트윗을 전송할 수 있는 button 엘리먼트를 작성하세요. */}
               <button
                 className="tweetForm__submitButton"
+                value={정보목록}
                 onChange={handleButtonClick}
+                onClick={handleButtonClick}
               >
                 Tweet
               </button>
@@ -76,7 +93,7 @@ const Tweets = () => {
       <ul className="tweets">
         {/* TODO : 하나의 트윗이 아니라, 주어진 트윗 목록(dummyTweets) 갯수에 맞게 보여줘야 합니다. */}
         {/* <Tweet tweet={dummyTweets[0]} /> */}
-        {tweets.map((el) => (
+        {정보목록.map((el) => (
           <Tweet tweet={el} key={el.id} />
         ))}
       </ul>
