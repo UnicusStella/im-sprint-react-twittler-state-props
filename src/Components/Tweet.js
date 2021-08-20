da9838ef916d6631cb6540656933f4a7a5ba2802
UnicusStella <unicusstella@gmail.com>
2021-08-20 19:34:52 +0900

    Tweet Components Complete

--- src/Components/Tweet.js
+++ src/Components/Tweet.js
@@ -2,22 +2,26 @@ import React from 'react';
 import './Tweet.css';
 
 const Tweet = ({ tweet }) => {
-  const parsedDate = new Date(tweet.createdAt).toLocaleDateString('ko-kr');
+  const { id, picture, username, content, createdAt } = tweet;
+  const parsedDate = new Date(createdAt).toLocaleDateString('ko-kr');
 
   return (
-    <li className="tweet" id={tweet.id}>
+    <li className="tweet" id={id}>
       <div className="tweet__profile">
-        <img src={tweet.picture} />
+        <img src={picture} />
       </div>
       <div className="tweet__content">
         <div className="tweet__userInfo">
           <div className="tweet__userInfo--wrapper">
             {/* TODO : 유져 이름이 있어야 합니다. */}
             {/* TODO : 트윗 생성 일자가 있어야 합니다. parsedDate를 이용하세요. */}
+            <span className="tweet__username">{username}</span>
+            <span className="tweet__createdAt">{parsedDate}</span>
           </div>
         </div>
         <div className="tweet__message">
-          TODO : 트윗 메세지가 있어야 합니다.
+          {/* TODO : 트윗 메세지가 있어야 합니다. */}
+          {content}
         </div>
       </div>
     </li>
